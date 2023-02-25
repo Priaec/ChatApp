0856324ea79a188de7e7f8edeb8a9a6e5f196925
Priaec <101358256+Priaec@users.noreply.github.com>
2023-02-25 12:51:45 -0800

    Chatting works, ONLY LOCALHOST

--- client.js
+++ client.js
@@ -1,12 +1,37 @@
 /*** client.js ***/
 const io = require("socket.io-client");
 const socket = io("http://localhost:3000");
-var nickname = null;
+const readline = require('readline');
+let nickname = null;
 console.log("Connecting to the server...");
 socket.on("connect", () => {
     nickname = process.argv[2];
-    console.log("[INFO]: Welcome %s", nickname);
+    console.log("[User]: Welcome %s", nickname);
 });
 socket.on("disconnect", (reason) => {
     console.log("[INFO]: Client disconnected, reason: %s", reason);
 });
+
+const rl = readline.createInterface(
+    {
+        input: process.stdin,
+        output: process.stdout
+    }
+);
+//when a user types on the running command line, this function is executed
+rl.on('line', (input)=>{
+    if(true === input.startsWith('b;')){
+        let str = input.slice(2);
+        socket.emit('broadcast', 
+        {
+            "sender": nickname,
+            "action": 'broadcast',
+            "msg": str
+        });
+    }
+});
+
+//send the data to the server, this can also be used to insert data into a DB
+socket.on('broadcast', (data)=>{
+    console.log('%s', data.msg);
+})
--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const io = require('socket.io')(port);
 
 console.log('Server is listening on port: %d', port);
 
-//
+//when a new member is connected to out server, run this function
 io.of("/").on("connect", (socket) => {
-    console.log("\nA client connected");
-    
+    console.log("A client connected");
+    //when a user disconnects, display the reason why
     socket.on("disconnect", (reason) => {
         console.log("\nA client disconnected, reason: %s", reason);
         console.log("Number of clients: %d", io.of('/').server.engine.clientsCount);
+    });
+});
+
+//
+io.of('/').on("connect", (socket)=>{
+    //
+    socket.on('broadcast', (data)=>{
+        console.log("\n%s", data);
+        socket.broadcast.emit("broadcast", data);
     });
 });
